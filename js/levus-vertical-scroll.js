3f46c4caad81d36479e3624a51f0fe761d7992e8
duncan <lvivduncan@gmail.com>
2020-07-24 17:36:55 +0300

    finish

--- js/levus-vertical-scroll.js
+++ js/levus-vertical-scroll.js
@@ -1,78 +1,34 @@
-const
-    ul = '.levus-vertical-scroll ul',
-    li = '.levus-vertical-scroll li',
-    toUp = '.levus-vertical-scroll .up',
-    toDown = '.levus-vertical-scroll .down';
-
-const scroll = document.querySelector(ul);
+const scroll = document.querySelector('.levus-vertical-scroll ul');
 
 // отримуємо список елементів
-let elements = scroll.querySelectorAll(li);
+let elements = scroll.querySelectorAll('.levus-vertical-scroll li');
 
-// клонуємо
-elements.forEach(element => {
-    const clone = element.cloneNode(true);
-    scroll.append(clone);
-});
-
-// отримуємо новий список
-elements = scroll.querySelectorAll(li);
-
-// кількість елементів
-const length = elements.length - 1;
-
-// 1 елемент
-const number = 1;
-
-// тимчасовий масив
-const sizes = [];
-
-elements.forEach((element, i) => {
-    sizes.push(i * number - number);
-});
-
-// висота основного блоку
-const height = scroll.offsetHeight;
-
-// кількість елементів
-const numerosity = height / 60;
-
-// z-index елемента
-let zIndex = 0;
-
-move();
-
-// розміщуємо елементи
-function move() {
-    elements.forEach((item, i) => {
-        item.style.position = 'absolute';
-        item.style.top = 0;
-        item.style.left = 0;
-
-        item.style.transform = `translateY(${sizes[i] * 60}px)`;
-
-        if (sizes[i] < -1 || sizes[i] > 14) zIndex = 1;
-        else zIndex = 3;
-
-        item.style.zIndex = zIndex;
-    });
-}
-
-const up = document.querySelector(toUp);
-const down = document.querySelector(toDown);
+const up = document.querySelector('.levus-vertical-scroll .up');
+const down = document.querySelector('.levus-vertical-scroll .down');
 
 // показуємо кнопки
 up.style.display = down.style.display = 'block';
 
+// scroll to bottom
 up.addEventListener('click', () => {
-    const first = sizes.pop();
-    sizes.unshift(first);
-    move();
+    const last = scroll.lastElementChild;
+    scroll.prepend(last);
+    scroll.style.transform = 'translateY(-60px)';
+    scroll.style.transition = '';
+    setTimeout(() => {
+        scroll.style.transform = 'translateY(0)';
+        scroll.style.transition = 'all .5s';
+    }, 500);
 });
 
-// scroll to right
+// scroll to top
 down.addEventListener('click', () => {
-    const last = sizes.shift();
-    sizes.push(last);
-    move();
+    const first = scroll.firstElementChild;
+    scroll.append(first);
+    scroll.style.transform = 'translateY(60px)';
+    scroll.style.transition = '';
+    setTimeout(() => {
+        scroll.style.transform = 'translateY(0)';
+        scroll.style.transition = 'all .5s';
+    }, 500);
 });
