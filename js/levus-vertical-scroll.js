a617f3159a036e4b754a946179f4b2201c6444a2
duncan <lvivduncan@gmail.com>
2020-07-24 18:36:54 +0300

    added a check

--- js/levus-vertical-scroll.js
+++ js/levus-vertical-scroll.js
@@ -1,5 +1,19 @@
 const scroll = document.querySelector('.levus-vertical-scroll ul');
 
+// отримуємо список елементів
+let elements = scroll.querySelectorAll('.levus-vertical-scroll li');
+
+console.log(elements.length)
+
+// якщо елементів менше 15, то клонуємо всі
+if (elements.length <= 14) {
+    elements.forEach(element => {
+        const clone = element.cloneNode(true);
+        scroll.append(clone);
+    });
+    elements = scroll.querySelectorAll('.levus-vertical-scroll li');
+}
+
 const up = document.querySelector('.levus-vertical-scroll .up');
 const down = document.querySelector('.levus-vertical-scroll .down');
 
