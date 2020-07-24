68f17d4c3184ad715cdf03a619860b6ca8e4aab0
duncan <lvivduncan@gmail.com>
2020-07-24 21:49:35 +0300

    OOP version

--- js/levus-vertical-scroll.js
+++ js/levus-vertical-scroll.js
@@ -1,45 +1,57 @@
-const scroll = document.querySelector('.levus-vertical-scroll ul');
+class LevusVerticalScroll {
+    constructor(id) {
+        this.id = document.querySelector(id);
+        this.scroll = this.id.querySelector('ul');
+        this.elements = this.id.querySelectorAll('li');
+        this.up = this.id.querySelector('.up');
+        this.down = this.id.querySelector('.down');
+    }
 
-// отримуємо список елементів
-let elements = scroll.querySelectorAll('.levus-vertical-scroll li');
+    clone() {
+        if (this.elements.length <= 14) {
 
-console.log(elements.length)
+            this.elements.forEach(element => {
+                const clone = element.cloneNode(true);
+                this.scroll.append(clone);
+            });
+            this.elements = this.scroll.querySelectorAll('.levus-vertical-scroll li');
+        }
+    }
 
-// якщо елементів менше 15, то клонуємо всі
-if (elements.length <= 14) {
-    elements.forEach(element => {
-        const clone = element.cloneNode(true);
-        scroll.append(clone);
-    });
-    elements = scroll.querySelectorAll('.levus-vertical-scroll li');
+    buttons() {
+        this.up.style.display = this.down.style.display = 'block';
+    }
+
+    arrow() {
+        this.up.addEventListener('click', () => {
+            const last = this.scroll.lastElementChild;
+            this.scroll.prepend(last);
+            this.scroll.style.transform = 'translateY(-60px)';
+            this.scroll.style.transition = '';
+            setTimeout(() => {
+                this.scroll.style.transform = 'translateY(0)';
+                this.scroll.style.transition = 'all .5s';
+            }, 300);
+        });
+
+        this.down.addEventListener('click', () => {
+            const first = this.scroll.firstElementChild;
+            this.scroll.append(first);
+            this.scroll.style.transform = 'translateY(60px)';
+            this.scroll.style.transition = '';
+            setTimeout(() => {
+                this.scroll.style.transform = 'translateY(0)';
+                this.scroll.style.transition = 'all .5s';
+            }, 300);
+        });
+    }
+
+    init() {
+        this.clone();
+        this.buttons();
+        this.arrow();
+    }
+
 }
 
-const up = document.querySelector('.levus-vertical-scroll .up');
-const down = document.querySelector('.levus-vertical-scroll .down');
-
-// показуємо кнопки
-up.style.display = down.style.display = 'block';
-
-// scroll to bottom
-up.addEventListener('click', () => {
-    const last = scroll.lastElementChild;
-    scroll.prepend(last);
-    scroll.style.transform = 'translateY(-60px)';
-    scroll.style.transition = '';
-    setTimeout(() => {
-        scroll.style.transform = 'translateY(0)';
-        scroll.style.transition = 'all .5s';
-    }, 500);
-});
-
-// scroll to top
-down.addEventListener('click', () => {
-    const first = scroll.firstElementChild;
-    scroll.append(first);
-    scroll.style.transform = 'translateY(60px)';
-    scroll.style.transition = '';
-    setTimeout(() => {
-        scroll.style.transform = 'translateY(0)';
-        scroll.style.transition = 'all .5s';
-    }, 500);
-});
+document.querySelector('#scroll') && new LevusVerticalScroll('#scroll').init();
---
+++ js/levus-vertical-scroll.noOOP.js
@@ -0,0 +1,43 @@
+const scroll = document.querySelector('.levus-vertical-scroll ul');
+
+// отримуємо список елементів
+let elements = scroll.querySelectorAll('.levus-vertical-scroll li');
+
+// якщо елементів менше 15, то клонуємо всі
+if (elements.length <= 14) {
+    elements.forEach(element => {
+        const clone = element.cloneNode(true);
+        scroll.append(clone);
+    });
+    elements = scroll.querySelectorAll('.levus-vertical-scroll li');
+}
+
+const up = document.querySelector('.levus-vertical-scroll .up');
+const down = document.querySelector('.levus-vertical-scroll .down');
+
+// показуємо кнопки
+up.style.display = down.style.display = 'block';
+
+// scroll to bottom
+up.addEventListener('click', () => {
+    const last = scroll.lastElementChild;
+    scroll.prepend(last);
+    scroll.style.transform = 'translateY(-60px)';
+    scroll.style.transition = '';
+    setTimeout(() => {
+        scroll.style.transform = 'translateY(0)';
+        scroll.style.transition = 'all .5s';
+    }, 300);
+});
+
+// scroll to top
+down.addEventListener('click', () => {
+    const first = scroll.firstElementChild;
+    scroll.append(first);
+    scroll.style.transform = 'translateY(60px)';
+    scroll.style.transition = '';
+    setTimeout(() => {
+        scroll.style.transform = 'translateY(0)';
+        scroll.style.transition = 'all .5s';
+    }, 300);
+});
